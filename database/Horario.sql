0c66707b3cd79d1f677558bb8a3321e99133872a
Manolo Perez <manolope338@gmail.com>
2018-04-06 20:53:15 -0500

    Dominio y DB

---
+++ database/Horario.sql
@@ -0,0 +1,27 @@
+
+CREATE DATABASE Horario;
+
+USE Horario;
+
+CREATE TABLE EE(
+    idEE int NOT NULL auto_increment,
+    nombre varchar(255) NOT NULL,
+    profesor varchar(255) NOT NULL,
+    color varchar(7) NOT NULL,
+    primary key (idEE)
+);
+
+CREATE TABLE Clase(
+    idClase int NOT NULL auto_increment,
+    idEE int NOT NULL,
+    dia varchar(15) NOT NULL,
+    horaInicio time NOT NULL,
+    horaFin time NOT NULL,
+    salon varchar(50) NOT NULL,
+    nota text,
+    primary key (idClase, idEE)
+);
+
+ALTER TABLE Clase ADD CONSTRAINT
+    fk_clase foreign key (idEE)
+    REFERENCES EE (idEE);
